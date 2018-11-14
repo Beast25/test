97bf52b9651ade77e00c22e87a53bce84ae09e9c
ArmtechBusinessSolutions <ArmtechBusinessSolutions@absimac.local>
2018-11-14 11:22:52 +0500

    Initial Commit

---
+++ test!@#/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test!@#
+//
+//  Created by Ajay kumar on 14/11/2018.
+//  Copyright © 2018 Appilogix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
